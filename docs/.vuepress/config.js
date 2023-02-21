a1ee5f9a36e41a608e1c0538b8a8c18b37a6417f
bh <bhchen_task@163.com>
2023-02-21 21:44:35 +0800

    d

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  title: 'orokin-van',
+  description: 'Van',
+  theme: 'reco',
+  base: '/blogs/',
+  locales: {
+    "/": {
+      lang: "zh-CN",
+    },
+  },
+  themeConfig: {
+    type: "blog",
+    author: "Van",
+    logo: "/avatar.jpg",
+    authorAvatar: "/avatar.jpg",
+    nav: [{
+        text: "首页",
+        link: "/"
+      },
+    ],
+    // 博客配置
+    blogConfig: {
+      category: {
+        location: 2, // 在导航栏菜单中所占的位置，默认2
+        text: "博客", // 默认文案 “分类”
+      },
+      tag: {
+        location: 4, // 在导航栏菜单中所占的位置，默认4
+        text: "Tag", // 默认文案 “标签”
+      },
+    },
+  }
+}
